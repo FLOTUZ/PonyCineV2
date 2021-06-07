45a23a31b4ed6ddeb769d789cb6b1043e84a0ba2
Emmanuel <flotuz10@gmail.com>
2021-06-07 14:26:06 -0500

    Se Hizo el resumen de la compra

--- app/src/main/java/com/example/ponycinev2/ui/ResumenCompra.kt
+++ app/src/main/java/com/example/ponycinev2/ui/ResumenCompra.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.example.ponycinev2.R
 import com.example.ponycinev2.databinding.ResumenCompraFragmentBinding
 
 class ResumenCompra : Fragment() {
 
-    private lateinit var viewModel: GlobalViewModel
+    private val viewModel: GlobalViewModel by activityViewModels()
 
     private var _binding: ResumenCompraFragmentBinding? = null
     private val binding get() = _binding!!
@@ -24,6 +28,26 @@ class ResumenCompra : Fragment() {
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        viewModel.getPeliculaseleccionada.observe(viewLifecycleOwner, Observer {
+            binding.imagenPeliculaResumen.setImageResource(it.img)
+            binding.nombrePeliculaResumen.text = it.nombre
+        })
+
+        viewModel.getHorarioSeleccionado.observe(viewLifecycleOwner, Observer {
+            binding.horaResumen.text = it.hora
+            binding.fechaResumen.text = it.fecha
+        })
+
+        viewModel.getAsientosSeleccionados.observe(viewLifecycleOwner, Observer {
+            binding.asientosResumen.text = it.toString()
+        })
+
+        binding.verBoletos.setOnClickListener {
+            findNavController().navigate(R.id.action_resumenCompra_to_miPerfil)
+        }
+    }
 
 }
